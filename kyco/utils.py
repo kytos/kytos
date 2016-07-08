81b5c64c019338d14562422cc7a5868ef3e176ca
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-08 10:54:24 -0300

    Improving some docstrings and names.

--- kyco/utils.py
+++ kyco/utils.py
@@ -9,6 +9,7 @@ APP_MSG = "[App %s] %s | ID: %02d | R: %02d | P: %02d | F: %s"
 
 
 def start_logger():
+    """Starts the loggers, both the Kyco and the KycoNApp"""
     general_formatter = logging.Formatter('%(asctime)s - %(levelname)s '
                                           '[%(name)s] %(message)s')
     app_formatter = logging.Formatter('%(asctime)s - %(levelname)s '
@@ -33,62 +34,87 @@ def start_logger():
     return controller_log
 
 
-class KycoApp(metaclass=ABCMeta):
+class KycoNApp(metaclass=ABCMeta):
+    """Base class for any KycoNApp to be developed."""
     def __init__(self):
-		"""
-			Go through all of the instance methods and selects those that have 
-			the events attribute, then creates a dict containing the event_name
-			and the list of methods that are responsible for handling such event
-		"""
-    	self._listeners = {}
+        """Go through all of the instance methods and selects those that have
+        the events attribute, then creates a dict containing the event_name
+        and the list of methods that are responsible for handling such event.
 
-		handler_methods = [getattr(self,method_name) for method_name in 
-						   dir(self) if callable(getattr(self,method_name))
-						   and hasattr(method_name,'events')]
-		
-		for method in handler_methods:
-			for event_name in method.events:
-				if event_name not in self._listeners:
-					self._listeners[event_name] = []
-				self._listeners[event_name].append(method)
+        At the end, the setUp method is called as a complement of the init
+        process.
+        """
+        self._listeners = {}
 
+        handler_methods = [getattr(self, method_name) for method_name in
+                           dir(self) if callable(getattr(self, method_name))
+                           and hasattr(method_name, 'events')]
+
+        for method in handler_methods:
+            for event_name in method.events:
+                if event_name not in self._listeners:
+                    self._listeners[event_name] = []
+                self._listeners[event_name].append(method)
+        self.setUp()
 
     @abstractmethod
     def setUp(self):
+        """Replaces the 'init' method for the KycoApp subclass"""
         pass
 
 
-
-
-
-
 class listen_to(object):
-    """Decorator for App Events Listener"""
+    """Decorator for Event Listeners methods.
+
+    This decorator should be used on methods, inside an APP, to define which
+    type of event the method will handle. With this, we will be able to
+    'schedule' the app/method to receive an event when a new event is
+    registered on the controller buffers.
+
+    The decorator will add an attribute to the method called 'events', that
+    will be a list of the events that the method will handle.
+
+    Example of usage:
+
+    .. code-block:: python3
+
+        class MyAppClass(KycoApp):
+            @listen_to('KycoMessageIn')
+            def my_handler_of_message_in(self, event):
+                # Do stuff here...
+
+            @listen_to('KycoMessageOut')
+            def my_handler_of_message_out(self, event):
+                # Do stuff here...
+
+            @listen_to('KycoMessageIn', 'KycoMessageOut')
+            def my_stats_handler_of_any_message(self, event):
+                # Do stuff here...
+    """
     def __init__(self, event, *events):
+        """Initial setup of handler methods.
+
+        This will be called when the class is created.
+        It need to have at least one event type (as string).
+
+        Args:
+            event (str): String with the name of a event to be listened to.
+            events (str): other events to be listened to.
+        """
         if callable(event):
             raise Exception("You need to pass at least one eventType")
         self.events = [event]
         self.events.extend(events)
 
-    def __call__(self, f):
-
+    def __call__(self, handler):
+        """Just return the handler method with the events attribute"""
         def wrapped_func(*args):
-            return f(*args)
+            return handler(*args)
         wrapped_func.events = self.events
         return wrapped_func
 
 
-class ExampleApp(KycoApp):
-
-    def __call__(self, f):
-
-        def wrapped_func(*args):
-            return f(*args)
-        wrapped_func.events = self.events
-        return wrapped_func
-
-
-class ExampleApp(KycoApp):
+class ExampleApp(KycoNApp):
     def setUp(self):
         self.nome = "APP"
 

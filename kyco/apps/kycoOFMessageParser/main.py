eb72e9b951570006167278c5664632cdb69c735d
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-11 18:41:12 -0300

    Adding a core APP to parse Raw OF Messages

---
+++ kyco/apps/kycoOFMessageParser/main.py
@@ -0,0 +1,51 @@
+"""This App is the responsible for unpacking a RAW OpenFlow Message.
+
+It will receive a unpacked header and the binary data of the rest
+of the message.
+From the header, it will identify the OpenFlow Message Type.
+Then a new message (of that type) will be instantiated and then the binary
+data will be unpacked into that message.
+
+After that, the it will generate a new KycoEvent, relative to the message type,
+and put it into the MessageInBuffer of the Controller."""
+
+import logging
+
+from pyof.v0x01.common.utils import new_message_from_header
+
+# from kyco.utils import APP_MSG
+from kyco.core.events import KycoMessageIn
+from kyco.utils import KycoCoreNApp
+from kyco.utils import ListenTo
+
+log = logging.getLogger('kytos[A]')
+
+
+class Main(KycoCoreNApp):
+    """Unpack a RAW OpenFlow Message and generate a KycoEvent."""
+    msg_in_buffer = True
+    name = 'Raw OpenFlow Message Parser'
+
+    def set_up(self, **kwargs):
+        self.app_id = kwargs['app_id'] if 'app_id' in kwargs else 0
+
+    @ListenTo('KycoRawOpenFlowMessageIn')
+    def handle_raw_message_in(self, raw_event):
+        """Handle a RawEvent and generate a KycoMessageIn event.
+
+        Args:
+            event (KycoRawOpenFlowMessageIn): RawOpenFlow Messag to be unpacked
+        """
+        message = new_message_from_header(raw_event.context.header)
+        message.unpack(raw_event.context.buffer)
+
+        # TODO: Do we need other informations from the network packet?
+        context = {'connection': raw_event.context.connection,
+                   'message': message}
+
+        event = KycoMessageIn(context)
+
+        self.add_to_msg_in_buffer(event)
+
+    def shutdown(self):
+        pass

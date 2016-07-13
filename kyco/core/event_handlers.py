9dae381140f959aaba75acf4fef541cff039abb6
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-13 13:39:13 -0300

    Some fixes on event_handlers
    Fix #21

--- kyco/core/event_handlers.py
+++ kyco/core/event_handlers.py
@@ -2,16 +2,23 @@
 import logging
 
 from struct import unpack
-
+from kyco.core.events import KycoRawOpenFlowMessageIn
 log = logging.getLogger('Kyco')
 
 
-def raw_event_handler(raw_buffer, msg_in_buffer, app_buffer):
+def raw_event_handler(listeners, raw_buffer, msg_in_buffer, app_buffer):
     log.info("Raw Event Handler started")
     while True:
         event = raw_buffer.get()
+        log.debug("\n#######################################################\n"
+                  "                 RawEvent handler called\n"
+                  "#######################################################\n")
         log.debug('KycoRawEvent read...')
-        log.debug(event.context)
+        # log.debug("%s: %s", event.context, event.content)
+        if isinstance(event, KycoRawOpenFlowMessageIn):
+            for listener in listeners['KycoRawOpenFlowMessageIn']:
+                listener(event)
+
         # data = unpack('!B', data)[0]
         # addr = writer.get_extra_info('peername')
         # log.debug("[Message Handler] New OF Message '%s' received from %s",
@@ -20,36 +27,46 @@ def raw_event_handler(raw_buffer, msg_in_buffer, app_buffer):
         # msg_in_buffer.put(message)
 
 
-def msg_in_event_handler(msg_in_buffer):
+def msg_in_event_handler(listeners, msg_in_buffer):
     log.info("Message In Event Handler started")
     # msg = "[Message Processor] MessageInEvenet %s"
     while True:
         event = msg_in_buffer.get()
-        log.debug(event.context)
+        log.debug("\n#######################################################\n"
+                  "                 MsgInEvent handler called\n"
+                  "#######################################################\n")
+        log.debug("%s: %s", event.context, event.content)
         # apps = self._events_listeners[0]
         # executor = ThreadPoolExecutor(max_workers=len(apps))
         # self.executors.append(executor)
         # [executor.submit(app.handle_event, message_event) for app in apps]
 
 
-def msg_out_event_handler(msg_out_buffer):
+def msg_out_event_handler(listeners, msg_out_buffer):
     log.info("Message Out Event Handler started")
     # msg = "[Message Processor] MessageInEvenet %s"
     while True:
         event = msg_out_buffer.get()
-        log.debug(event.context)
+        log.debug("\n#######################################################\n"
+                  "                 MsgOutEvent handler called\n"
+                  "#######################################################\n")
+        log.debug("%s: %s", event.context, event.content)
+
         # apps = self._events_listeners[0]
         # executor = ThreadPoolExecutor(max_workers=len(apps))
         # self.executors.append(executor)
         # [executor.submit(app.handle_event, message_event) for app in apps]
 
 
-def app_event_handler(app_buffer):
+def app_event_handler(listeners, app_buffer):
     log.info("App Event Handler started")
     # msg = "[Message Processor] MessageInEvenet %s"
     while True:
         event = app_buffer.get()
-        log.debug(event.context)
+        log.debug("\n#######################################################\n"
+                  "                 AppEvent handler called\n"
+                  "#######################################################\n")
+        log.debug("%s: %s", event.context, event.content)
         # apps = self._events_listeners[0]
         # executor = ThreadPoolExecutor(max_workers=len(apps))
         # self.executors.append(executor)

105025130f3e8f1e8bf36aaca6a90e8c2f479bfb
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-15 17:12:36 -0300

    Improved Handshake Core App.
    Going up to features_request message
    
    Done-by: Diego Rabatone Oliveira (@diraol)
    Done-by: Raphael M. Cobe (@raphaelmcobe)

--- kyco/core/apps/OFCore/main.py
+++ kyco/core/apps/OFCore/main.py
@@ -2,13 +2,18 @@
 
 import logging
 
+from random import randint
+
 from pyof.v0x01.symmetric.hello import Hello
+from pyof.v0x01.symmetric.echo_reply import EchoReply
+from pyof.v0x01.controller2switch.features_request import FeaturesRequest
 from pyof.v0x01.common.header import Type
 from pyof.v0x01.common.utils import new_message_from_header
 
 from kyco.core import events
 from kyco.utils import KycoCoreNApp
 from kyco.utils import ListenTo
+
 
 log = logging.getLogger('kytos[A]')
 
@@ -48,6 +53,8 @@ class Main(KycoCoreNApp):
 
         if message.header.message_type == Type.OFPT_HELLO:
             event = events.KycoMessageInHello(content)
+        elif message.header.message_type == Type.OFPT_ECHO_REQUEST:
+            event = events.KycoMessageInEchoRequest(content)
         else:
             event = events.KycoMessageIn(content)
 
@@ -78,7 +85,40 @@ class Main(KycoCoreNApp):
         event_out = events.KycoMessageOutHello(content)
 
         self.add_to_msg_out_buffer(event_out)
+
+        features_request = FeaturesRequest(xid=randint(1, 100))
+
+        content = {'connection': message_event.content.get('connection'),
+                   'message': features_request}
+
+        features_request_out = events.KycoMessageOutFeaturesRequest(content)
+
+        self.add_to_msg_out_buffer(features_request_out)
+
         log.debug('OpenFlowMessageOutHello event added to msg_out buffer')
+
+    @ListenTo('KycoMessageInEchoRequest')
+    def handle_echo_request_event(self, message_event):
+        """ Handle Echo Request Event by Generating an Echo Reply Answer
+
+         Args:
+             event (KycoMessageInEchoRequest): Received Event
+
+        """
+        echo_request = message_event.content.get('message')
+
+        log.debug("Echo Request message read")
+
+        echo_reply = EchoReply(xid=echo_request.header.xid)
+
+        content = {'connection': message_event.content.get('connection'),
+                   'message': echo_reply}
+
+        event_out = events.KycoMessageOutEchoReply(content)
+
+        self.add_to_msg_out_buffer(event_out)
+
+        log.debug("OpenflowMessageOutEchoReply event added tp msg_out bufffer")
 
     def shutdown(self):
         pass

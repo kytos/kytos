08db55906dd2fed10dc2d486e115625e3aaab00c
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-15 14:34:24 -0300

    Added some new exceptions

--- kyco/core/exceptions.py
+++ kyco/core/exceptions.py
@@ -1,5 +1,31 @@
 """Kyco Core-Defined Exceptions"""
 
+# Buffers exceptions
+
+
+class KycoEventException(Exception):
+    """Event related exceptions"""
+    def __init__(self, message="KycoEvent exception", event=None):
+        self.message = message
+        self.event = event
+
+    def __str__(self):
+        message = self.message
+        if self.event:
+            message += ". EventType: " + type(self.event)
+        return message
+
+
+class KycoWrongEventType(KycoEventException):
+    """Exception related to EventType.
+
+    When related to buffers, it means that the EventType is not allowed on
+    that buffer."""
+    pass
+
+# Exceptions related  to NApps
+
+
 class KycoNAppException(Exception):
     """Exception raised on a KycoNApp"""
     def __init__(self, message="KycoNApp exception"):

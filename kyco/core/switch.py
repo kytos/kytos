ebe6bcbaf5fa6049e97b2faf9d6b88ac2a4191ae
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-27 15:25:24 -0300

    Improving KycoSwitch attributes.
    We will save the socket, connection_id (ip, port) and the openflow version that
    the switch will 'talk'.

--- kyco/core/switch.py
+++ kyco/core/switch.py
@@ -9,9 +9,13 @@ log = logging.getLogger('Kyco')
 class KycoSwitch(object):
     """This is the main class related to Switches modeled on Kyco."""
 
-    def __init__(self, dpid, socket=None):
+    def __init__(self, dpid, socket, connection_id, ofp_version='0x01',
+                 features=None):
         self.dpid = dpid
         self.socket = socket
+        self.connection_id = connection_id  # (ip, port)
+        self.ofp_version = ofp_version
+        self.features = features
 
     def send(self, data):
         if not self.socket:
@@ -19,12 +23,14 @@ class KycoSwitch(object):
             raise Exception("This switch is not connected")
         self.socket.send(data)
 
-    def save_connection(self, socket):
-        if self.socket:
+    def save_connection(self, socket, connection_id):
+        if self.socket.is_connected():
             # TODO: raise proper exception
-            message = "The switch {} already have an alive socket connection"
-            raise Exception(message.format(self.dpid))
+            error_message = ("Kyco already have a connected switch with "
+                             "dpid {}")
+            raise Exception(error_message.format(self.dpid))
         self.socket = socket
+        self.connection_id = connection_id
 
     def disconnect(self):
         try:
@@ -33,6 +39,7 @@ class KycoSwitch(object):
             pass
 
         self.socket = None
+        self.connection_id = None
 
     def is_connected(self):
         try:

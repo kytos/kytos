065e294ab5ffdae691fe903e98872c3ff64fc9ef
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-07-28 14:55:05 -0300

    Improving setup.py with python-openflow reference for tests/docs

--- setup.py
+++ setup.py
@@ -1,24 +1,82 @@
-import os
+"""Setup script.
+
+Run "python3 setup --help-commands" to list all available commands and their
+descriptions.
+"""
 import sys
+from subprocess import call
+
 from setuptools import setup, find_packages, Command
 
 
 class Doctest(Command):
+    """Run Sphinx doctest."""
+
     if sys.argv[-1] == 'test':
-        print("Running docs make and make doctest")
-        os.system("make doctest -C docs/")
+        print('Running examples in documentation')
+        call('make doctest -C docs/', shell=True)
+
+
+class Linter(Command):
+    """Run several code linters."""
+
+    description = 'Run many code linters. It may take a while'
+    user_options = []
+
+    def __init__(self, *args, **kwargs):
+        """Define linters and a message about them."""
+        super().__init__(*args, **kwargs)
+        self.linters = ['pep257', 'pyflakes', 'mccabe', 'isort', 'pep8',
+                        'pylint']
+        self.extra_msg = 'It may take a while. For a faster version (and ' \
+                         'less checks), run "quick_lint".'
+
+    def initialize_options(self):
+        """For now, options are ignored."""
+        pass
+
+    def finalize_options(self):
+        """For now, options are ignored."""
+        pass
+
+    def run(self):
+        """Run pylama and radon."""
+        files = 'tests setup.py pyof'
+        print('running pylama with {}. {}'.format(', '.join(self.linters),
+                                                  self.extra_msg))
+        cmd = 'pylama -l {} {}'.format(','.join(self.linters), files)
+        call(cmd, shell=True)
+        print('Low grades (<= C) for Cyclomatic Complexity:')
+        call('radon cc --min=C ' + files, shell=True)
+        print('Low grades (<= C) for Maintainability Index:')
+        call('radon mi --min=C ' + files, shell=True)
+
+
+class FastLinter(Linter):
+    """Same as Linter, but without the slow pylint"""
+
+    description = 'Same as "lint", but much faster (no pylama_pylint).'
+
+    def __init__(self, *args, **kwargs):
+        """Remove slow linters and redefine the message about the rest."""
+        super().__init__(*args, **kwargs)
+        self.linters.remove('pylint')
+        self.extra_msg = 'This a faster version of "lint", without pylint. ' \
+                         'Run the slower "lint" after solving these issues:'
+
 
 setup(name='Kyco - Kytos Controller',
-      version='0.1',
-      description='Kytos Controller for OpenFlow Protocol',
+      version='1.1.0a0',
+      description='Controller for OpenFlow Protocol from the Kytos project',
       url='http://github.com/kytos/kyco',
       author='Kytos Team',
       author_email='of-ng-dev@ncc.unesp.br',
       license='MIT',
       test_suite='tests',
       scripts=['bin/kyco'],
-      packages=find_packages(exclude=[]),
+      packages=find_packages(exclude=['tests']),
       cmdclass={
-        'doctests': Doctest
+          'lint': Linter,
+          'quick_lint': FastLinter
       },
       zip_safe=False)

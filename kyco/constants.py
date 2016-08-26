82cc118431b04ec4f9c4568479df0cfb98e1adac
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-08-26 15:52:25 -0300

    Adding a new constant on the controller

--- kyco/constants.py
+++ kyco/constants.py
@@ -1,2 +1,7 @@
+"""This module contains the main constants from Kyco.
+
+This constantes may be overridden by values passed on the controller
+instantiation"""
 # Min time (seconds) to send a new EchoRequest to a switch
-POOLING_TIME = 3
+POOLING_TIME = 1
+CONNECTION_TIMEOUT = 15

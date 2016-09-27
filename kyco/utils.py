be5cb10a62506f3e7fcf311ed51250d510bf5b6e
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2016-09-27 18:33:36 -0300

    Define a default logging format with thread name
    Close #92

--- kyco/utils.py
+++ kyco/utils.py
@@ -100,28 +100,10 @@ def run_on_thread(method):
 
 def start_logger():
     """Starts the loggers, both the Kyco and the KycoNApp"""
-    fmt = '%(asctime)s - %(levelname)s [%(name)s] %(message)s'
+    fmt = '%(asctime)s - %(levelname)s [%(name)s] (%(threadName)s) %(message)s'
     # fmt += '\n           %(module)s - %(funcName)s - %(lineno)d'
-    general_formatter = logging.Formatter(fmt)
-    app_formatter = logging.Formatter(fmt)
-
-    controller_console_handler = logging.StreamHandler()
-    controller_console_handler.setLevel(logging.DEBUG)
-    controller_console_handler.setFormatter(general_formatter)
-
-    app_console_handler = logging.StreamHandler()
-    app_console_handler.setLevel(logging.DEBUG)
-    app_console_handler.setFormatter(app_formatter)
-
-    controller_log = logging.getLogger('Kyco')
-    controller_log.setLevel(logging.DEBUG)
-    controller_log.addHandler(controller_console_handler)
-
-    app_log = logging.getLogger('KycoNApp')
-    app_log.setLevel(logging.DEBUG)
-    app_log.addHandler(app_console_handler)
-
-    return controller_log
+    logging.basicConfig(format=fmt, level=logging.DEBUG)
+    return logging.getLogger('Kyco')
 
 
 class KycoNApp(Thread, metaclass=ABCMeta):

c7a6c88e76865ea5189871750160fddacf0cbc40
Beraldo Leal <beraldo.ncc@gmail.com>
2016-10-26 15:06:39 -0200

    fixed dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,6 +35,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-express');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('server', ['express', 'dev', 'watch']);
-    grunt.registerTask('dev', ['sass']);
+    grunt.registerTask('server', ['express', 'sass', 'watch']);
 }

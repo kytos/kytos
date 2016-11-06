40c13c98b85b2463d8128e17c20b21e9bed0e6dd
José Luiz Coe <joseluizcoe@gmail.com>
2016-11-06 01:41:31 -0200

    Ajustes em Context”

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -1,11 +1,30 @@
 ;(function() {
 
-$(".terminal").kterminal();
+  $(".terminal").kterminal();
 
-$("[type='checkbox'], [type='radio']").bootstrapSwitch();
+  $("[type='checkbox'], [type='radio']").bootstrapSwitch();
 
-$("a,div").focus(function(){
-  $(this).blur();
-})
+  $("a,div").focus(function(){
+    $(this).blur();
+  })
+
+
+  // Change the selector if needed
+  var $table = $('table.scroll'),
+      $bodyCells = $table.find('tbody tr:first').children(),
+      colWidth;
+
+  // Adjust the width of thead cells when window resizes
+  $(window).resize(function() {
+      // Get the tbody columns width array
+      colWidth = $bodyCells.map(function() {
+          return $(this).width();
+      }).get();
+
+      // Set the width of thead columns
+      $table.find('thead tr').children().each(function(i, v) {
+          $(v).width(colWidth[i]);
+      });
+  }).resize(); // Trigger resize handler
 
 }());

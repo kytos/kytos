084156408a5eb4bd383919f7129f72ac9790bae1
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-11-07 17:37:26 -0200

    Improving log system. Autoscroll and some div on log messages

--- source/static/js/kytos-logs.js
+++ source/static/js/kytos-logs.js
@@ -1,25 +1,40 @@
 ;(function() {
   console.log("kytos-logs started")
-  var $logs = $("#tab_logs").text('')
   // Method used to request a websocket and append in tab_logs terminal.
-  function LogWebSocketReceive()
-  {
-     if ("WebSocket" in window)
-     {
-        var ws = new WebSocket("ws://localhost:8765");
+  function LogWebSocketReceive() {
+    if ("WebSocket" in window) {
+      var ws = new WebSocket("ws://localhost:8765");
 
-        ws.onmessage = function (evt)
-        {
-           var received_msg = evt.data;
-           received_msg = received_msg.replace(/\n/g,'<br>')
-           $logs.append(received_msg)
-        };
-     }
-     else
-     {
-        console.log("WebSocket NOT supported by your Browser!");
-     }
+      ws.onmessage = function (evt) {
+        var received_msgs = evt.data;
+        $.each(received_msgs.split('\n'), function(index, msg) {
+          if (msg) {
+            add_log_message(msg, 'controller')
+          }
+        })
+      };
+    } else {
+      console.log("WebSocket NOT supported by your Browser!");
+    }
   }
 
   setInterval(function(){ LogWebSocketReceive() }, 2000);
+
+  function resize_log_tab() {
+    $('#tab_logs').height($('.terminal-body').height() - 25);
+  }
+
+  $('#tab_logs').on('resize', resize_log_tab).trigger('resize');
+
 }());
+
+function add_log_message(msg, src_tag) {
+  if ($('#tab_logs div.log_message').length >= 500) {
+    $('#tab_logs').find('#tab_logs:first').remove();
+  }
+  $('<div/>', {
+      text: msg,
+      "class": 'log_message ' + src_tag
+  }).appendTo('#tab_logs');
+  $('#tab_logs').scrollTop($('#tab_logs').get(0).scrollHeight);
+}

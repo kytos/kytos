6989c6ca2d2abb1dc525ccc0267b6f087cd6dd31
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-11-08 17:44:45 -0200

    Adding a js file for general settings

---
+++ source/static/js/kytos-settings.js
@@ -0,0 +1,7 @@
+var default_settings = {
+  'show_unused_interfaces': true,
+  'show_disconnected_hosts': true,
+  'show_topology': true,
+  'map_center': [-97.8445676, 35.3437248],
+  'map_zoom': 4
+}

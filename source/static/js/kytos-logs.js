dfbd9c2eebb2935c0fcc9a0817f580510f429e3f
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-11-11 22:42:58 -0200

    Adding statuses check and led update on kytos-log

--- source/static/js/kytos-logs.js
+++ source/static/js/kytos-logs.js
@@ -1,26 +1,36 @@
 ;(function() {
   console.log("kytos-logs started")
   // Method used to request a websocket and append in tab_logs terminal.
-  function LogWebSocketReceive() {
-    if ($('#enable_log')[0].checked) {
-      if ("WebSocket" in window) {
-        var ws = new WebSocket(log_server_url);
+  ws_led = $('.nav.sidebar .websocket-status');
 
+  function LogWebSocketReceive() {
+    if ("WebSocket" in window) {
+      var ws = new WebSocket(log_server_url)
+      console.log(ws.readyState);
+      ws.onopen = function(evt) {
+        ws_led.addClass('status-online');
+        ws_led.removeClass('status-offline');
+      };
+      ws.onerror = function(evt) {
+        ws_led.removeClass('status-online');
+        ws_led.addClass('status-offline');
+      }
+      if ($('#enable_log')[0].checked) {
         ws.onmessage = function (evt) {
           var received_msgs = evt.data;
           $.each(received_msgs.split('\n'), function(index, msg) {
-            if (msg) {
-              add_log_message(msg, 'controller')
-            }
+            if (msg) { add_log_message(msg, 'controller')}
           })
         };
-      } else {
-        console.log("WebSocket NOT supported by your Browser!");
       }
+    } else {
+      //console.log("WebSocket NOT supported by your Browser!");
+      $('#enable_log').prop('checked', false).change();
+      ws_led.removeClass('status-online status-offline')
     }
   }
 
-  setInterval(function(){ LogWebSocketReceive() }, 2000);
+  setInterval(LogWebSocketReceive, 2000);
 
   function resize_log_tab() {
     $('#tab_logs').height($('.terminal-body').height() - 25);

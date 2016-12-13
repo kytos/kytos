508751322d450ffad161569da3393af5d8d5ba76
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2016-12-13 15:22:20 -0200

    Add solarized-like colors to code-blocks
    FIX #156

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,7 @@ add_function_parentheses = True
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = None
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []

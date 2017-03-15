20f970778488e91414b8161bd4f317eb094d4d93
Beraldo Leal <beraldo.ncc@gmail.com>
2017-03-15 16:17:39 -0300

    fixed multiple projects with same kytos module

--- kytos/__init__.py
+++ kytos/__init__.py
@@ -3,4 +3,7 @@
 Kyco is used to handle low level openflow messages using python-openflow
 library.
 """
+from pkgutil import extend_path
+
 __version__ = "1.1.0b1.dev1"
+__path__ = extend_path(__path__, __name__)

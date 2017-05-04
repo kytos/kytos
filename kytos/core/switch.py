de47b344dd8996cc8a13024adb24990a5c1ca920
Erick Vermot <erick.vermot@gmail.com>
2017-05-04 12:57:19 -0300

    fixes switch.is_connected method to check if connection exists first.

--- kytos/core/switch.py
+++ kytos/core/switch.py
@@ -377,7 +377,8 @@ class Switch(object):
 
     def is_connected(self):
         """Verify if the switch is connected to a socket."""
-        return self.connection.is_connected() and self.is_active()
+        return (self.connection is not None and
+                self.connection.is_connected() and self.is_active())
 
     def update_connection(self, connection):
         """Update switch connection.

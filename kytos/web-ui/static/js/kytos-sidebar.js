4a7cf59b7ae3248a3cd8c66fc011f417099a8379
ValessioBrito <contato@valessiobrito.com.br>
2017-06-13 12:37:15 -0300

    Add sidebar right

--- kytos/web-ui/static/js/kytos-sidebar.js
+++ kytos/web-ui/static/js/kytos-sidebar.js
@@ -2,8 +2,8 @@
   $(".right-nav-toggle").click(function(e){
       e.stopPropagation();
       e.preventDefault();
-      $('.nav.sidebar').toggleClass('hidden');
-      $('.navbar-header').toggleClass('hidden');
+      $('#sidebar-right').toggleClass('hidden');
+//    $('.navbar-header').toggleClass('hidden');
       $('#terminal').toggleClass('with-out-left-bar')
   });
 }());

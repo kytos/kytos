4efdcdf58c12e777a91f79f43a2a1c9d3835c494
Macartur Sousa <macartur.sc@gmail.com>
2017-12-15 17:24:54 -0200

    Update version to 2018.1b1.dev0

--- kytos/core/metadata.py
+++ kytos/core/metadata.py
@@ -2,7 +2,7 @@
 
 The present metadata is intended to be used mainly on the setup.
 """
-__version__ = '2017.2b2.post1'
+__version__ = '2018.1b1.dev0'
 __author__ = 'Kytos Team'
 __author_email__ = 'devel@lists.kytos.io'
 __license__ = 'MIT'

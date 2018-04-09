336fa1981d939427ea15f384773f40bc7065d563
Humberto Diógenes <hdiogenes@gmail.com>
2018-04-09 18:02:37 -0300

    Remove unused constant (fix #80)
    POOLING_TIME is declared but not used in Kytos or NApps.

--- kytos/core/constants.py
+++ kytos/core/constants.py
@@ -1,10 +1,8 @@
 """Module with the main constants from Kytos.
 
-This constantes may be overridden by values passed on the controller
+These constants may be overridden by values passed on the controller
 instantiation.
 """
-# Min time (seconds) to send a new EchoRequest to a switch
-POOLING_TIME = 3
 CONNECTION_TIMEOUT = 70
-#: FLOOD_TIMEOUT in microseconds
+# FLOOD_TIMEOUT in microseconds
 FLOOD_TIMEOUT = 100000

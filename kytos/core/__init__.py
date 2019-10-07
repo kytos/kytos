12ae134b9298895e0a285d5af85bef2c446411b0
Carlos Magno <cmagnobarbosa@gmail.com>
2019-10-06 23:53:34 -0300

    Decorator @Authenticated works.

--- kytos/core/__init__.py
+++ kytos/core/__init__.py
@@ -3,10 +3,12 @@ from kytos.core.controller import Controller
 from kytos.core.events import KytosEvent
 from kytos.core.logs import NAppLog
 from kytos.core.napps import KytosNApp, rest
+from kytos.core.authb1 import Authenticated
 
 from .metadata import __version__
 
-__all__ = ('Controller', 'KytosEvent', 'KytosNApp', 'log', 'rest',
+__all__ = ('Authenticated', 'Controller', 'KytosEvent', 'KytosNApp', 'log',
+            'rest',
            '__version__')
 
 # Kept lowercase to be more user friendly.

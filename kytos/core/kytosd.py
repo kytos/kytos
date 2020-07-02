cf3c0dae39c1fc1cadd1be4f46f78149fc813b63
slinksoft <slinkpro@gmail.com>
2020-07-01 22:28:54 -0400

    Attempted to fix pylint errors, possibly ongoing

--- kytos/core/kytosd.py
+++ kytos/core/kytosd.py
@@ -14,6 +14,7 @@ import daemon
 from IPython.terminal.embed import InteractiveShellEmbed
 from IPython.terminal.prompts import Prompts, Token
 from traitlets.config.loader import Config
+
 from kytos.core import Controller
 from kytos.core.config import KytosConfig
 from kytos.core.metadata import __version__

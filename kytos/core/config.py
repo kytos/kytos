37fd9b9253d5cab869b71c43c805e8e4d4a5dede
Carlos Magno <cmagnobarbosa@gmail.com>
2020-10-29 09:33:56 -0300

    Move auth token expiry time to the kytos.conf file
    Today, the authentication token expiration time is set in hard code in
    the authentication module. This commit solves this problem by moving this
    configuration to the kytos.conf file.

--- kytos/core/config.py
+++ kytos/core/config.py
@@ -108,6 +108,7 @@ class KytosConfig():
                         'foreground': False,
                         'protocol_name': '',
                         'enable_entities_by_default': False,
+                        'token_expiration_time': 180,
                         'debug': False}
 
         """
@@ -129,6 +130,7 @@ class KytosConfig():
                     'napps_pre_installed': [],
                     'authenticate_urls': [],
                     'vlan_pool': {},
+                    'token_expiration_time': 180,
                     'debug': False}
 
         options, argv = self.conf_parser.parse_known_args()
@@ -169,6 +171,7 @@ class KytosConfig():
         options.port = int(options.port)
         options.api_port = int(options.api_port)
         options.protocol_name = str(options.protocol_name)
+        options.token_expiration_time = int(options.token_expiration_time)
 
         result = options.enable_entities_by_default in ['True', True]
         options.enable_entities_by_default = result

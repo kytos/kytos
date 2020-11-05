58188829c3915a263dacf6db25b28485639c5357
Bárbara Boechat <barbs.boechat@gmail.com>
2020-11-05 18:03:51 -0300

    Update make_blueprints_table.py
    String formats improved, bp_tables.rst created and include it on index.rst. Also the call for the new script to the Makefile has been added

--- docs/blueprints/make_blueprints_table.py
+++ docs/blueprints/make_blueprints_table.py
@@ -1,65 +1,66 @@
 import re
 import os
 import glob
-
-def create_toc_tree():
-    main_title = '\n' + '#' * 10 + '\nBlueprints\n' + '#' * 10 + '\n'
-    sub_title = '\nTable of Blueprints\n\n'
-    
-    file_header = [':orphan:\n']
-    file_header.append(main_title)
-    file_header.append(sub_title)
-    
-    toc_tree_text = ['.. toctree::\n', '  :glob:\n', '  :hidden:\n','\n', '  EP*\n\n',]
-    
-    file_header = ''.join(file_header)
-    toc_tree_text = ''.join(toc_tree_text)
-    
-    return file_header, toc_tree_text
-    
-    
+ 
+  
 def create_table(directory):
-    #Create the table header
+    # Create the table header and cells
     table_header = ''
-    table_header += '+' + '-' * 23 + '+' + '-' * 82 + '+' + '-' * 20 + '+\n'
-    table_header += '|' + ' Blueprint'  + ' ' * 13 + '|' + ' Title' + ' ' * 76 + '|' + ' Status' + ' ' * 13 + '|\n'
-    table_header += '+' + '=' * 23 + '+' + '=' * 82 + '+' + '=' * 20 + '+\n'
-    
-    
     table_cell = ''
+    
+    bps_rst = [] 
+    bps_titles = []
+    bps_status = []
+    
+    max_len_title = -1
+    max_len_status = -1
+    max_len_bps = -1
+    
     for fp in glob.glob(f'{directory}/EP*.rst'):
         split_dir = ''.join(fp.split('./blueprints/'))
         bp_rst = ''.join(split_dir.split('.rst'))
-    
+        bps_rst.append(f" :doc:`{bp_rst}<{bp_rst}/>` ")
+        if max_len_bps < len(bps_rst[-1]): max_len_bps = len(bps_rst[-1])
+        
         with open(fp) as origin_file:
-            title = 'Metadata not define for :Title:'
-            status = 'Metadata not define for :Status:'
+            title = ''
+            status = ''
             for line in origin_file:
                 if re.findall(r':Title:', line):
                     title = ''.join(line.split(':Title:'))
-                    title = ''.join(title.split("\n"))
+                    bps_titles.append(''.join(title.split("\n")))
+                    if max_len_title < len(title): max_len_title = len(title)
+                    
                 if re.findall(r':Status:', line):
                     status = ''.join(line.split(':Status:'))
-                    status = ''.join(status.split("\n"))
+                    bps_status.append(''.join(status.split("\n")))
+                    if max_len_status < len(status): max_len_status = len(status)
                     break
-                
-        title_space = 82 - (len(title))
-        status_space = 20 - (len(status))
-        table_cell += '|' + ' :doc:`'+ bp_rst + '<' + bp_rst + '/>` '  + ' ' + '|' + title + ' ' * title_space + '|' + status + ' ' * status_space + '|\n'
-        table_cell += '+' + '-' * 23 + '+' + '-' * 82 + '+' + '-' * 20 + '+\n'
+    
+    th_title_len = (max_len_title - len(' Title'))
+    th_status_len = (max_len_status - len(' Status'))
+    th_bps_len = (max_len_bps - len(' Blueprint'))
+    
+    table_header += f"+{'-' * max_len_bps}+{'-' * max_len_title}+{'-' * max_len_status}+\n"
+    table_header += f"|{' Blueprint'}{' ' * th_bps_len}|{' Title'}{' ' * th_title_len}|{' Status'}{' ' * th_status_len}|\n"
+    table_header += f"+{'=' * max_len_bps}+{'=' * max_len_title}+{'=' * max_len_status}+\n"
+           
+    for i in range(len(bps_rst)):
+        title_space = max_len_title - len(bps_titles[i])
+        status_space = max_len_status - len(bps_status[i])
+        bps_space = max_len_bps - len(bps_rst[i])
+        
+        table_cell += f"|{bps_rst[i]}{' ' * bps_space}|{bps_titles[i]}{' ' * title_space}|{bps_status[i]}{' ' * status_space}|\n"
+        table_cell += f"+{'-' * max_len_bps}+{'-' * max_len_title}+{'-' * max_len_status}+\n"
         
     return table_header + table_cell
 
 
 def write_new_index_rst(directory):
-    file_header, toc_tree_text = create_toc_tree()
     blueprints_table = create_table(directory)
     
-    with open(f'{directory}/index.rst', 'w') as fp:
-        fp.write(file_header)
-        fp.write(toc_tree_text)
+    with open(f'{directory}/bp_table.rst', 'w') as fp:
         fp.write(blueprints_table)
-
 
 if __name__ == '__main__':
     write_new_index_rst('./blueprints')    

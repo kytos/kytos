6078db2d95cc37dfc87d78defaf25708ba4e8d78
josemauro <z.mauro@live.com>
2021-05-04 15:51:30 -0300

    Add authenticated decorator to shutdown API command (#1231)
    Add a decorator to require authentication when shutting down remotely
    via REST API. Besides that, updates the unit tests of 'shutdown_api'
    method and replace the exception raised when the attribute 'content'
    has an invalid value.
    
    Co-authored-by: Carlos Magno <cmagnobarbosa@gmail.com>

--- tests/unit/test_core/test_api_server.py
+++ tests/unit/test_core/test_api_server.py
@@ -54,21 +54,28 @@ class TestAPIServer(unittest.TestCase):
 
         mock_exit.assert_called()
 
-    @patch('kytos.core.api_server.request')
-    def test_shutdown_api(self, mock_request):
+    @patch('kytos.core.auth.request')
+    @patch('kytos.core.auth.jwt.decode', return_value=True)
+    def test_shutdown_api(self, _, mock_request):
         """Test shutdown_api method."""
-        mock_request.host = 'localhost:8181'
 
+        mock_request.headers = {'Authorization': 'Bearer 123'}
         self.api_server.shutdown_api()
 
         self.api_server.server.stop.assert_called()
 
-    @patch('kytos.core.api_server.request')
-    def test_shutdown_api__error(self, mock_request):
+    @patch('kytos.core.auth.jsonify')
+    @patch('kytos.core.auth.request')
+    def test_shutdown_api__error(self, mock_request, mock_jsonify):
         """Test shutdown_api method to error case."""
-        mock_request.host = 'any:port'
 
+        mock_request.headers = {'Authorization': None}
         self.api_server.shutdown_api()
+
+        exc_msg = "The attribute 'content' has an invalid value 'None'."
+        msg = f"Token not sent or expired: {exc_msg}"
+
+        mock_jsonify.assert_called_with({"error": msg})
 
         self.api_server.server.stop.assert_not_called()
 

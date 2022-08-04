976b18bca4e6abc4ca9e77ea3018b82b5c927d67
David Ramirez <davramir@fiu.edu>
2022-08-04 10:05:23 -0400

    Added in tests for apm_decorator

--- tests/unit/test_core/test_logger_decorators.py
+++ tests/unit/test_core/test_logger_decorators.py
@@ -1,9 +1,10 @@
 """Test the decorators for the loggers"""
 from logging.handlers import QueueHandler
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock, patch
 
-from kytos.core.logger_decorators import queue_decorator, root_decorator
+from kytos.core.logger_decorators import (apm_decorator, queue_decorator,
+                                          root_decorator)
 
 
 class DummyLoggerClass:
@@ -28,6 +29,12 @@ class DummyLoggerClass:
             return True
         return False
     # pylint: enable=invalid-name
+
+
+for method_name in ['debug', 'info', 'warning', 'error',
+                    'exception', 'critical', 'fatal', 'log']:
+    setattr(DummyLoggerClass, method_name,
+            MagicMock(__name__=method_name, __module__='logging'))
 
 
 class RootDecoratorTest(TestCase):
@@ -89,3 +96,54 @@ class QueueDecoratorTest(TestCase):
 
     def tearDown(self):
         pass
+
+
+class APMDecoratorTest(TestCase):
+    """Test the APM logger decorator"""
+
+    def setUp(self):
+        """Create decorated class for tests"""
+        self.decorated_class = apm_decorator(DummyLoggerClass)
+
+    @patch('kytos.core.apm.execution_context')
+    def test_decorated_logger(self, mock_context):
+        """Test the decorated logger."""
+
+        transaction = MagicMock()
+
+        mock_context.get_transaction.return_value = transaction
+
+        name = 'test'
+        level = 3
+        logger = self.decorated_class(name, level)
+        self.assertEqual(logger.name, name)
+        self.assertEqual(logger.level, level)
+
+        log_module = 'logging'
+        log_methods = ['debug', 'info', 'warning', 'error',
+                       'exception', 'critical', 'fatal', 'log']
+        span_type = 'logging'
+
+        for meth_name in log_methods:
+            # Setup Mocks
+            base_mock_method = getattr(DummyLoggerClass, meth_name)
+            transaction.begin_span = MagicMock()
+            transaction.end_span = MagicMock()
+
+            # Construct args
+            msg = f'Logging with {meth_name}'
+            if meth_name == 'log':
+                args = [6, msg]
+            else:
+                args = [msg]
+            method = getattr(logger, meth_name)
+
+            # Call
+            method(*args)
+
+            # Test Conditions
+            transaction.begin_span \
+                       .assert_called_with(f'{log_module}.{meth_name}',
+                                           span_type)
+            base_mock_method.assert_called_with(logger, *args)
+            transaction.end_span.assert_called()

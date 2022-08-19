a4377621916b1ecd7d89cd647d1c5c0d7f595c65
Vinicius Arcanjo <vindasil@fiu.edu>
2022-08-19 17:08:19 -0300

    Refactored unit tests and cover safe guard

--- tests/unit/test_core/test_interface.py
+++ tests/unit/test_core/test_interface.py
@@ -203,6 +203,10 @@ class TestInterface(unittest.TestCase):
         is_success = self.iface.use_tag(tag)
         self.assertTrue(is_success)
 
+        # Test sanity safe guard
+        self.assertFalse(self.iface.use_tag(None))
+        self.assertFalse(None not in self.iface.available_tags)
+
     def test_enable(self):
         """Test enable method."""
         self.iface.switch = MagicMock()

0d3579e4292bdd4366b7f7d7b7d08a732ce4eca7
Aldo Ortega <aorte090@fiu.edu>
2022-10-25 12:16:04 -0400

    Deleted `run_kytos.py`

--- run_kytos.py
+++
@@ -1,6 +0,0 @@
-import cmd
-import os
-
-
-cmd = "sudo ./docker/scripts/add-etc-hosts.sh; export MONGO_USERNAME=alopalao; export MONGO_PASSWORD=123; docker-compose up -d; kytosd -f --database mongodb"
-os.system(cmd)

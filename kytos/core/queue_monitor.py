b7bfd269875b794ff0627b6babeadf0b8b577b26
Vinicius Arcanjo <vindasil@fiu.edu>
2024-02-01 10:55:04 -0300

    feat: updated queue_monitor and shipped more unit tests

--- kytos/core/queue_monitor.py
+++ kytos/core/queue_monitor.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import logging
+import math
 from typing import Callable, Optional
 from collections import deque
 from uuid import uuid4
 from pydantic.dataclasses import dataclass
 from pydantic import Field
 from datetime import datetime, timedelta, timezone
+from kytos.core.exceptions import KytosCoreException
 from kytos.core.helpers import now
 
 LOG = logging.getLogger(__name__)
@@ -23,30 +25,52 @@ class QueueData:
 
 
 class QueueMonitorWindow:
-
     """QueueMonitorWindow."""
 
     def __init__(
         self,
         name: str,
+        min_hits: int,
         delta_secs: int,
         min_size_threshold: int,
-        min_hits: int,
         qsize_func: Callable[[], int],
+        log_at_most_n=0,
     ) -> None:
         """QueueMonitorWindow."""
         self.name = name
-        self.deque: deque[QueueData] = deque()
+        self.min_hits = min_hits
         self.delta_secs = delta_secs
         self.min_size_threshold = min_size_threshold
-        self.min_hits = min_hits
+        self.qsize_func = qsize_func
+        self.log_at_most_n = log_at_most_n
+
+        self.deque: deque[QueueData] = deque()
         self._default_last_logged = datetime(
             year=1970, month=1, day=1, tzinfo=timezone.utc
         )
         self._last_logged: datetime = self._default_last_logged
         self._tasks: set[asyncio.Task] = set()
-        self.qsize_func = qsize_func
         self._sampling_freq_secs = 1
+        self._validate()
+
+    def _validate(self) -> None:
+        """Validate QueueMonitorWindow."""
+        positive_attrs = (
+            "min_hits",
+            "delta_secs",
+            "min_size_threshold",
+        )
+        for attr in positive_attrs:
+            val = getattr(self, attr)
+            if val < 0:
+                raise KytosCoreException(
+                    f"{attr}: {val} must be positive"
+                )
+        ratio = self.min_hits / self.delta_secs
+        if ratio > 1:
+            raise KytosCoreException(
+                f"min_hits/delta_secs: {ratio} must be <= 1 "
+            )
 
     def __repr__(self) -> str:
         """Repr."""
@@ -56,7 +80,7 @@ class QueueMonitorWindow:
             else None
         )
         return (
-            f"QueueMonitorWindow(name={self.name}, len={len(self.deque)}, "
+            f"QueueMonitor(name={self.name}, len={len(self.deque)}, "
             f"last_logged={last_logged}, "
             f"min_hits={self.min_hits}, "
             f"min_size_threshold={self.min_size_threshold}, "
@@ -80,7 +104,8 @@ class QueueMonitorWindow:
         try:
             while True:
                 self._try_to_append(QueueData(size=self.qsize_func()))
-                self._try_to_log_stats()
+                records = self._try_to_log_stats()
+                self._try_to_log_at_most_n_records(records)
                 await asyncio.sleep(self._sampling_freq_secs)
         except asyncio.CancelledError:
             pass
@@ -93,15 +118,36 @@ class QueueMonitorWindow:
         self.deque.append(queue_data)
         return queue_data
 
-    def _try_to_log_stats(self) -> None:
+    def _try_to_log_at_most_n_records(self, records: list[QueueData]) -> None:
+        """Try to log at most n records."""
+        if self.log_at_most_n <= 0 or not records:
+            return
+
+        for i in range(
+            0,
+            len(records),
+            math.ceil(len(records) / self.log_at_most_n),
+        ):
+            record = records[i]
+            msg = (
+                f"{self.name}, "
+                f"record[{i}]/[{len(records)}]: size: {record.size}, "
+                f"at: {record.created_at}"
+            )
+            LOG.warning(msg)
+
+    def _try_to_log_stats(self) -> list[QueueData]:
         """Try to log stats."""
         self._popleft_passed_records()
+        records = []
         if (
             self.deque
             and len(self.deque) >= self.min_hits
             and self._last_logged + timedelta(seconds=self.delta_secs) <= now()
         ):
             first = self.deque.popleft()
+            if self.log_at_most_n > 0:
+                records.append(first)
             cur = first
             minv, maxv, size_acc, count = first.size, first.size, first.size, 1
             while (
@@ -113,20 +159,21 @@ class QueueMonitorWindow:
                 minv = min(minv, cur.size)
                 maxv = max(maxv, cur.size)
                 count += 1
+                if self.log_at_most_n > 0:
+                    records.append(first)
             avg = size_acc / count
             self._last_logged = cur.created_at
             msg = (
-                f"QueueMonitorWindow alert: {self.name}, counted: {count}, "
+                f"{self.name}, counted: {count}, "
                 f"min size: {minv}, max size: {maxv}, avg: {avg}, "
                 f"first at: {first.created_at}, last at: {cur.created_at},"
                 f" delta seconds: {self.delta_secs}, min_hits: {self.min_hits}"
             )
             LOG.warning(msg)
+        return records
 
     def _popleft_passed_records(self) -> None:
         """Pop left passed records."""
         delta = self.delta_secs
-        while (
-            self.deque and (now() - self.deque[0].created_at).seconds > delta
-        ):
+        while self.deque and (now() - self.deque[0].created_at).seconds > delta:
             self.deque.popleft()
--- tests/unit/test_core/test_queue_monitor.py
+++ tests/unit/test_core/test_queue_monitor.py
@@ -1,5 +1,7 @@
-from kytos.core.queue_monitor import QueueMonitorWindow
+from kytos.core.queue_monitor import QueueMonitorWindow, QueueData
 from kytos.core.exceptions import KytosCoreException
+from kytos.core.helpers import now
+from datetime import timedelta
 
 import pytest
 
@@ -29,3 +31,43 @@ class TestQueueMonitor:
                 qsize_func=lambda: 1,
             )
         assert exc_str in str(exc)
+
+    def test_try_to_append(self) -> None:
+        """Test try to append."""
+        qmon = QueueMonitorWindow(
+            "app",
+            min_hits=2,
+            delta_secs=10,
+            min_size_threshold=128,
+            qsize_func=lambda: 1,
+        )
+        assert not qmon.deque
+        assert not qmon._try_to_append(QueueData(size=64))
+        assert not qmon.deque
+        assert qmon._try_to_append(QueueData(size=256))
+        assert len(qmon.deque) == 1
+
+    def test_popleft_passed_records(self) -> None:
+        """Test popleft passed records."""
+        delta_secs = 10
+        qmon = QueueMonitorWindow(
+            "app",
+            min_hits=2,
+            delta_secs=delta_secs,
+            min_size_threshold=128,
+            qsize_func=lambda: 1,
+        )
+        n_records = 5
+        for _ in range(n_records):
+            dt = (now() - timedelta(seconds=delta_secs + 1))
+            assert qmon._try_to_append(QueueData(size=256, created_at=dt))
+        assert len(qmon.deque) == n_records
+        qmon._popleft_passed_records()
+        assert not qmon.deque
+
+        # it shouldn't popleft now if delta_secs haven't passed
+        for _ in range(n_records):
+            assert qmon._try_to_append(QueueData(size=256, created_at=now()))
+        assert len(qmon.deque) == n_records
+        qmon._popleft_passed_records()
+        assert len(qmon.deque) == n_records
